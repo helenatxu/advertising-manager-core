c0381082daa548988dd8972611a3179dc23865d4
Helena Moreno Chacon <helenaenred@gmail.com>
2021-10-25 20:45:14 +0200

    Adds routes for banners

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get 'banners/index'
+  get 'banners/create'
+  get 'banners/update'
+  get 'banners/destroy'
+  get 'banners/show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
